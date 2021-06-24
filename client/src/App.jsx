622b4b94134047120ce634afcca4cbaf1a6967c5
Arquimides <arquimides@Arquimidess-MacBook-Air.local>
2021-06-24 16:53:59 -0400

    done-ish

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,12 +11,13 @@ import { Route, Switch } from 'react-router-dom'
 const App = () => {
   return (
     <div className="app">
+      <h1>Hello</h1>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/posts" component={Posts} />
+        {/* <Route exact path="/posts" component={Posts} />
         <Route path="/add-post" component={PostCreate} />
         <Route exact path="/posts/:id/edit" component={PostEdit} />
-        <Route exact path="/posts/:id" component={PostDetail} />
+        <Route exact path="/posts/:id" component={PostDetail} /> */}
       </Switch>
     </div>
   )
--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,24 +1,15 @@
-import React from 'react'
-import './App.css'
-import Home from './screens/Home/Home'
-import Posts from './screens/Posts/Posts'
-import PostCreate from './screens/PostCreate/PostCreate'
-import PostEdit from './screens/PostEdit/PostEdit'
-import PostDetail from './screens/PostDetail/PostDetail'
-import { Route, Switch } from 'react-router-dom'
+import './Layout.css'
+import Nav from '../Nav/Nav'
+import Footer from '../Footer/Footer'
 
-const App = () => {
-  return (
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/posts" component={Posts} />
-        <Route path="/add-post" component={PostCreate} />
-        <Route exact path="/posts/:id/edit" component={PostEdit} />
-        <Route exact path="/posts/:id" component={PostDetail} />
-      </Switch>
+const Layout = (props) => (
+    <div className='layout'>
+        <Nav />
+        <div className="layout-children">
+            {props.children}
+        </div>
+        <Footer />
     </div>
-  )
-}
+)
 
-export default App
+export default Layout
